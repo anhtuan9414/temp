8ac7cd8b8ee593216ede7b84d47c6e46257ecca2
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-04-25 21:32:20 +0700

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+pkill pm2
+pkill astrominer
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install v10
 nvm use v10
 npm i pm2 -g
+pm2 delete all
 wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_generic_amd64_linux.tar.gz
 tar -xf astrominer-V1.9_generic_amd64_linux.tar.gz
 rm miner.sh
