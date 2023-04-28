445252a130545a814ec52520ca4ec35f380bc3e4
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-04-28 10:18:41 +0700

    Update start_miner_pkt.sh

--- start_miner_pkt.sh
+++ start_miner_pkt.sh
@@ -8,9 +8,9 @@ nvm install v10
 nvm use v10
 npm i pm2 -g
 pm2 delete all
-wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 && chmod +x packetcrypt-v0.5.1-linux_amd64
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64 && chmod +x packetcrypt-v0.5.2-linux_amd64
 rm miner.sh
-echo "./packetcrypt-v0.5.1-linux_amd64 ann -p $1 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com" >> miner.sh
+echo "./packetcrypt-v0.5.2-linux_amd64 ann -p $1 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com" >> miner.sh
 pm2 start -f miner.sh
 pm2 logs
 echo "DONE"
