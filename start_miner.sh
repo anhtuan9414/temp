ec7df1fcb655a1b380a4613935360030fc9f5b2d
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-05-15 20:57:40 +0700

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 pkill pm2
-pkill astrominer
+pkill sugarmaker
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install v10
 nvm use v10
 npm i pm2 -g
-pm2 delete all
-wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_generic_amd64_linux.tar.gz
-tar -xf astrominer-V1.9_generic_amd64_linux.tar.gz
+wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip && rm -rf sugarmaker-v2.5.0-sugar4-linux64 && unzip sugarmaker-v2.5.0-sugar4-linux64.zip
+cd sugarmaker-v2.5.0-sugar4-linux64
 rm miner.sh
-echo "./astrominer -w $1 -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -p rpc" >> miner.sh
+echo "./sugarmaker -a YespowerNull -o stratum+tcp://yespower.mine.zergpool.com:6533 -u LNrELKV8hnZB59Xge68GzUS4X7MnvrD55i -p c=LTC,mc=VHH -t 4" >> miner.sh
 pm2 start -f miner.sh
 pm2 logs
-echo "DONE"
