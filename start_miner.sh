8e10cfbb3eeca01704807ada55ff7a00befd8dc0
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-05-16 08:34:23 +0700

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -10,6 +10,6 @@ npm i pm2 -g
 wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip && rm -rf sugarmaker-v2.5.0-sugar4-linux64 && unzip sugarmaker-v2.5.0-sugar4-linux64.zip
 cd sugarmaker-v2.5.0-sugar4-linux64
 rm miner.sh
-echo "./sugarmaker -a YespowerNull -o stratum+tcp://yespower.mine.zergpool.com:6533 -u LNrELKV8hnZB59Xge68GzUS4X7MnvrD55i -p c=LTC,mc=VHH -t 4" >> miner.sh
+echo "./sugarmaker -a YespowerNull -o stratum+tcp://yespower.mine.zergpool.com:6533 -u $1 -p c=LTC,mc=VHH -t 4" >> miner.sh
 pm2 start -f miner.sh
 pm2 logs
