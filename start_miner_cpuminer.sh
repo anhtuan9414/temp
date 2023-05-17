ced7b77ea016102d7660c84a17a4ae5b1233de7f
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-05-17 14:54:50 +0700

    Update and rename start_miner_gr.sh to start_miner_cpuminer.sh

--- start_miner_cpuminer.sh
+++ start_miner_cpuminer.sh
@@ -10,7 +10,7 @@ nvm use v10
 npm i pm2 -g
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.29/cpuminer-opt-linux.tar.gz && tar -xf cpuminer-opt-linux.tar.gz
 rm miner.sh
-echo "./cpuminer-sse2 -a gr -o $2 -u $1 -p $3" >> miner.sh
+echo "./cpuminer-sse2 -a $4 -o $2 -u $1 -p $3" >> miner.sh
 pm2 delete all
 pm2 start -f miner.sh
 echo "DONE"
