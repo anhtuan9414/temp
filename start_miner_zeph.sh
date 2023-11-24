7adeed9c7770ba86ccd82521e117bbdc497df7d6
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-11-24 18:55:37 +0700

    Update start_miner_zeph.sh

--- start_miner_zeph.sh
+++ start_miner_zeph.sh
@@ -4,9 +4,9 @@ sudo pkill SRBMiner-MULTI
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install v16
-nvm use v16
-npm i pm2 -g
+sudo nvm install v16
+sudo nvm use v16
+sudo npm i pm2 -g
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.6/SRBMiner-Multi-2-3-6-Linux.tar.xz && tar -xf SRBMiner-Multi-2-3-6-Linux.tar.xz && cd SRBMiner-Multi-2-3-6
 rm miner.sh
 threads=$(echo $(echo "$(grep -c ^processor /proc/cpuinfo)" | bc) | awk '{printf "%d\n",$1}')
