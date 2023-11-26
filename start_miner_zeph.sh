6954dfd1cafb628df89d18431cbcae49b0415f08
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-11-26 19:16:52 +0700

    Update start_miner_zeph.sh

--- start_miner_zeph.sh
+++ start_miner_zeph.sh
@@ -8,7 +8,7 @@ npm i pm2 -g
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.6/SRBMiner-Multi-2-3-6-Linux.tar.xz && tar -xf SRBMiner-Multi-2-3-6-Linux.tar.xz && cd SRBMiner-Multi-2-3-6
 rm miner.sh
 threads=$(echo $(echo "$(grep -c ^processor /proc/cpuinfo)" | bc) | awk '{printf "%d\n",$1}')
-echo "./SRBMiner-MULTI --algorithm randomx --disable-gpu --pool us-zephyr.miningocean.org:5462 --wallet ZEPHYR3S4EW5ELfHhnUvedX3WWqnuaPV5akQ2rGXtydyRkYZdNSPNrrfoATQZyALCeAAYeDJBDBXy9X23LaCaDY6a7pRjQa1dUm3a --password NA --cpu-threads $threads" >> miner.sh
+echo "sudo ./SRBMiner-MULTI --algorithm randomx --disable-gpu --pool us-zephyr.miningocean.org:5462 --wallet ZEPHYR3S4EW5ELfHhnUvedX3WWqnuaPV5akQ2rGXtydyRkYZdNSPNrrfoATQZyALCeAAYeDJBDBXy9X23LaCaDY6a7pRjQa1dUm3a --password NA --cpu-threads $threads" >> miner.sh
 pm2 start -f miner.sh
 pm2 log --nostream
 echo "DONE"
