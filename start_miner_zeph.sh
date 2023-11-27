33b5ebeafba746ce780c66e161ec718a195a3d54
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2023-11-27 11:55:17 +0700

    Update start_miner_zeph.sh

--- start_miner_zeph.sh
+++ start_miner_zeph.sh
@@ -9,7 +9,7 @@ pm2 delete all
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.6/SRBMiner-Multi-2-3-6-Linux.tar.xz && tar -xf SRBMiner-Multi-2-3-6-Linux.tar.xz && cd SRBMiner-Multi-2-3-6
 rm miner.sh
 threads=$(echo $(echo "$(grep -c ^processor /proc/cpuinfo)" | bc) | awk '{printf "%d\n",$1}')
-(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=2304" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=2304
+(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=3075" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=3075
 echo "sudo ./SRBMiner-MULTI --algorithm randomx --disable-gpu --pool us-zephyr.miningocean.org:5462 --wallet ZEPHYR3S4EW5ELfHhnUvedX3WWqnuaPV5akQ2rGXtydyRkYZdNSPNrrfoATQZyALCeAAYeDJBDBXy9X23LaCaDY6a7pRjQa1dUm3a --password NA --cpu-threads $threads" >> miner.sh
 pm2 start -f miner.sh
 pm2 log --nostream
