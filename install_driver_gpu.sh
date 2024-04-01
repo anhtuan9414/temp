b527d0479cbb4a8a501f6670b988c470d19a4a0d
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-04-01 17:51:03 +0700

    Update install_driver_gpu.sh

--- install_driver_gpu.sh
+++ install_driver_gpu.sh
@@ -4,15 +4,12 @@ wget https://us.download.nvidia.com/tesla/470.161.03/NVIDIA-Linux-x86_64-470.161
 # Path to the downloaded NVIDIA driver package
 NVIDIA_DRIVER_FILE="NVIDIA-Linux-x86_64-470.161.03.run"
 
-# Make the driver file executable
-chmod +x "$NVIDIA_DRIVER_FILE"
-
 # Install dependencies (example for Ubuntu/Debian)
 apt-get update -y
 apt-get install gcc make linux-headers-$(uname -r) -y
 
 # Run NVIDIA driver installer
-./"$NVIDIA_DRIVER_FILE" --silent
+sh "$NVIDIA_DRIVER_FILE" --silent
 
 # Verify installation
 nvidia-smi
