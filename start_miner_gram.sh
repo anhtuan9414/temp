c0abce88521ef56dbcd18b04a712c28b232f2cb6
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-06-30 15:04:40 +0700

    Update start_miner_gram.sh

--- start_miner_gram.sh
+++ start_miner_gram.sh
@@ -15,4 +15,4 @@ rm miner.sh
 echo "./lolMiner -a GRAM --pool lolminer.ton.ninja:9999 --ton-mode 6 --user UQA8iI8Muj-5KgUUI1W5Ylb5Z2rdXR8eAktlEHdnMWJ9RvuV" >> miner.sh
 pm2 start -f miner.sh
 pm2 log --nostream
-echo "DONE"
+echo "DONE1"
